f3a0c746d2b21f00327521a49dd37a01f437fb64
ANTOINE1128 <makdessyantoine@gmail.com>
2023-11-30 18:58:55 +0200

    Setup libraries and  redux

---
+++ src/components/reducers.js
@@ -0,0 +1,15 @@
+// reducers.js
+const initialState = {
+    // Your initial state goes here
+  };
+  
+  const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+      // Define your action types and corresponding state changes here
+      default:
+        return state;
+    }
+  };
+  
+  export default rootReducer;
+  
