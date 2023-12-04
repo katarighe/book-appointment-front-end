108f3c8e8657da15c6966179a76b4f2d202f1164
clarence <tegumclarence@gmail.com>
2023-12-04 18:02:53 +0100

    Refactor root rendering in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './components/store';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
