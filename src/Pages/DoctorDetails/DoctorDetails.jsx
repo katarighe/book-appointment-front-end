4cd1a3d18f9b44e6d4c6bece031671090f0ef71d
clarence <tegumclarence@gmail.com>
2023-12-11 14:30:14 +0100

    Added doctordetails

---
+++ src/Pages/DoctorDetails/DoctorDetails.jsx
@@ -0,0 +1,69 @@
+import { useSelector } from 'react-redux';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { selectDoctor } from '../../Redux/Features/doctorSlice';
+import './DoctorDetails.scss';
+import { BsCaretLeft } from 'react-icons/bs';
+
+function DoctorDetails() {
+  const { id } = useParams();
+  const { allDoctors } = useSelector(selectDoctor);
+  const navigate = useNavigate();
+  const { name, imageUrl, description, specialization, costPerDay, city } =
+    allDoctors.find((doc) => doc.id === id);
+
+  return (
+    <main className='doctorDetails'>
+      <section className='container d-flex flex-column flex-md-row justify-content-between'>
+        <article className='col-12 col-md-7'>
+          <figure className=''>
+            <img src={imageUrl} alt='' />
+          </figure>
+        </article>
+        <article className='col-12 col-md-4 d-flex flex-column align-items-end text-end'>
+          <section>
+            <h4 className='mb-5'> {name} </h4>
+
+            <div className='greyBg d-flex justify-content-between'>
+              <p> Bio: </p>
+              <p> {description} </p>
+            </div>
+
+            <div className='whiteBg d-flex justify-content-between'>
+              <p> Specialization: </p>
+              <p> {specialization} </p>
+            </div>
+
+            <div className='greyBg d-flex justify-content-between'>
+              <p> Cost Per Day: </p>
+              <p> ${costPerDay}/hr </p>
+            </div>
+
+            <div className='whiteBg d-flex justify-content-between'>
+              <p> City: </p>
+              <p> {city} </p>
+            </div>
+          </section>
+
+          <section className='mt-5'>
+            <Link to className='main-btn'>
+              {' '}
+              Book Appointment{' '}
+            </Link>
+          </section>
+        </article>
+      </section>
+      <section className='d-flex'>
+        <div
+          className='back '
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <BsCaretLeft />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default DoctorDetails;
