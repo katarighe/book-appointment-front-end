a736c378c9dad22618d27fb145f47aad49536316
rbd3 <rabedesanaandry01@gmail.com>
2023-12-11 18:49:28 +0300

    Add delete doctor page

--- src/Pages/ManageDocors/ManageDoctors.jsx
+++ src/Pages/ManageDocors/ManageDoctors.jsx
@@ -1,6 +1,49 @@
+import React, { useEffect } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { getDoctors } from '../../Redux/Features/doctorSlice';
 
 function ManageDoctors() {
-  return <div>ManageDoctors</div>;
+  const doctors = useSelector((state) => state.bookDoctorReducer.doctors);
+  const user = useSelector((state) => state.userReducer.userDetails);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!fetched) {
+      dispatch(getDoctors());
+      fetched = true;
+    }
+
+    if (user.role !== 'admin') {
+      navigate('/');
+    }
+  }, []);
+  const deleteDoc = (id) => {
+    dispatch(deleteDoctorThunk(id));
+  };
+
+  return (
+    <>
+      <section className="listContainer">
+        <div className="listDoctor">
+          {doctors ? doctors.map((doctor) => (
+            <div key={doctor.id} className="list">
+              <img src={doctor.imageUrl} alt="docs" className="docImage" />
+              <div className="delete">
+                <p className="docName">{doctor.name}</p>
+                <FaIcons.FaTrash
+                  role="button"
+                  className="dltBtn"
+                  onClick={() => { deleteDoc(doctor.id); }}
+                />
+              </div>
+            </div>
+          )) : <p>No Doctors 😔</p>}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default ManageDoctors;
