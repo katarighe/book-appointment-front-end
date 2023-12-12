c2392c6b6137c97f8631b3c39459cc5bbceca54c
rbd3 <rabedesanaandry01@gmail.com>
2023-12-12 10:52:16 +0300

    Update test

---
+++ src/--tests--/BookAppointment/BookApointment.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import BookAppointments from '../../Pages/BookAppointment/BookAppointments';
+import { selectDoctor } from '../../Redux/Features/doctorSlice';
+import { MemoryRouter } from 'react-router-dom';
+
+// Mock Redux store
+const mockStore = configureStore([]);
+
+describe('BookAppointments Component', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      doctorSlice: {
+        allDoctors: [
+          {
+            id: 1,
+            name: 'Dr. John Doe',
+            imageUrl: 'https://example.com/doctor.jpg',
+            specialization: 'Cardiologist',
+          },
+          // Add more doctor data as needed
+        ],
+        isLoading: false,
+      },
+    });
+  });
+
+  test('renders BookAppointments component', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+        <BookAppointments />
+        </MemoryRouter>
+        
+      </Provider>
+    );
+
+    expect(screen).toMatchSnapshot();
+  });
+});
---
+++ src/--tests--/BookAppointment/BookApointments2.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider, useDispatch } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import BookAppointments from '../../Pages/BookAppointment/BookAppointments';
+import { MemoryRouter } from 'react-router-dom';
+import { getAppointmentDetails } from '../../Redux/Features/appointmentSlice';
+
+// Mock Redux store
+const mockStore = configureStore([]);
+
+// Mock the useDispatch hook
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
+
+describe('BookAppointments Component', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      doctorSlice: {
+        allDoctors: [
+          {
+            id: 1,
+            name: 'Dr. John Doe',
+            imageUrl: 'https://example.com/doctor.jpg',
+            specialization: 'Cardiologist',
+          },
+        ],
+        isLoading: false,
+      },
+    });
+  });
+
+  test('handles doctor details link click', () => {
+    // Mock the useDispatch function
+    const mockDispatch = jest.fn();
+    useDispatch.mockReturnValue(mockDispatch);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <BookAppointments />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    // Simulate a click on the "Book Appointment" link
+    fireEvent.click(screen.getByText(/Book Appointment/i));
+
+    // Assert that the dispatch function is called with the correct action
+    expect(mockDispatch).toHaveBeenCalledWith(
+      getAppointmentDetails({
+        doctor_id: 1,
+        name: 'Dr. John Doe',
+        imageUrl: 'https://example.com/doctor.jpg',
+        specialization: 'Cardiologist',
+      })
+    );
+  });
+});
