c6e3a2b4a4ad2ad46e760d6ac4174fc8832c698c
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-15 11:14:57 +0200

    return vite.config.js the way it was

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import styleImport from 'vite-plugin-style-import';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    styleImport({
-      // Add your SCSS file paths here
-      libs: [
-        {
-          libraryName: 'antd',
-          esModule: true,
-          resolveStyle: (name) => `antd/es/${name}/style/index`,
-        },
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  server: {
-    open: true,
-  },
-});
+  plugins: [react()],
+})
