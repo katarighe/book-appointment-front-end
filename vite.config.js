3f0edd21d2deaaf80027cecf442882753ed48212
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-15 10:50:29 +0200

    Add config to vite.config.js file to enhanc Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+    assetsDir: 'assets', // Specify the assets directory
+  },
+  server: {
+    open: true, // Open the default browser when Vite starts
+  },
+});
